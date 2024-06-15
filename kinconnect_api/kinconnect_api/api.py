28a9902dffd42b6192f2385022fe74b92562638e
Nehil Jain <nehiljain@Queenies-MacBook-Pro-2.local>
2024-06-14 19:50:36 -0700

    WIP Mongo works

--- kinconnect_api/kinconnect_api/api.py
+++ kinconnect_api/kinconnect_api/api.py
@@ -1,9 +1,33 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
+from langchain_mongodb import MongoDBAtlasVectorSearch
 from typing import List
 from pymongo import MongoClient
+from langchain_fireworks import ChatFireworks
+from kinconnect_api.config import MONGO_CONNECTION_STRING, RAW_DATA_DIR, load_dotenv
+from kinconnect_api.features import get_full_profile
+import pandas as pd
+import pandas as pd
+from typing import List, Dict, Callable, Union
+import os
+from fastapi.middleware.cors import CORSMiddleware
 
+load_dotenv()
 app = FastAPI()
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
 class CareerEntry(BaseModel):
     company: str = Field(..., title="Company they worked at")
@@ -27,30 +51,57 @@ class ProfileModel(BaseModel):
     past_projects: List[ProjectEntry] = Field(..., title="Projects they have worked on")
     elevator_pitch: str = Field(..., title="Elevator pitch for the person for the event")
 
-client = MongoClient("mongodb://localhost:27017")
-db = client.kinconnect
+
+# Connect to MongoDB
+def get_db():
+    client = MongoClient(MONGO_CONNECTION_STRING)
+    return client['kinconnect']
+
+db = get_db()
+profiles_collection = db['demo_profiles']
+
+class QAPair(BaseModel):
+    question: str = Field(..., title="Question")
+    answer: str = Field(..., title="Answer")
+
+
 
 @app.post("/profiles/", response_model=ProfileModel)
-def create_profile(profile: ProfileModel):
-    if db.profiles.find_one({"name": profile.name}):
-        raise HTTPException(status_code=400, detail="Profile with this name already exists")
-    db.profiles.insert_one(profile.model_dump())
+def create_profile(form_submission: List[QAPair]):
+    print(form_submission)
+    profile =  get_full_profile(form_submission)
+    if profiles_collection.find_one({"name": profile.name}):
+        raise HTTPException(status_code=4000, detail="Profile with this name already exists")
+    # profiles_collection.insert_one(profile.model_dump())
     return profile
 
-@app.get("/profiles/{name}", response_model=ProfileModel)
-def read_profile(name: str):
-    profile = db.profiles.find_one({"name": name})
-    if profile is None:
-        raise HTTPException(status_code=404, detail="Profile not found")
-    return ProfileModel(**profile)
 
-@app.put("/profiles/{name}", response_model=ProfileModel)
-def update_profile(name: str, profile: ProfileModel):
-    updated_profile = db.profiles.find_one_and_update(
-        {"name": name},
+
+# # @app.get("/profiles/{name}", response_model=ProfileModel)
+# # def read_profile(name: str):
+# #     profile = profiles_collection.find_one({"name": name})
+# #     if profile is None:
+# #         raise HTTPException(status_code=404, detail="Profile not found")
+# #     return ProfileModel(**profile)
+
+@app.put("/profiles/", response_model=ProfileModel)
+def update_profile(profile: ProfileModel):
+    updated_profile = profiles_collection.find_one_and_update(
+        {"name": profile.name},
         {"$set": profile.model_dump()},
         return_document=True
     )
-    if updated_profile is None:
-        raise HTTPException(status_code=404, detail="Profile not found")
+    print(f"updated_profile: {updated_profile}")
+    # if updated_profile is None:
+    #     raise HTTPException(status_code=404, detail="Profile not found")
     return ProfileModel(**updated_profile)
+
+
+# # if __name__ == "__main__":
+# #     app.deploy("api")
+
+# # from fastapi import FastAPI
+
+# # app = FastAPI()
+
+
--- kinconnect_api/kinconnect_api/features.py
+++ kinconnect_api/kinconnect_api/features.py
@@ -1,29 +1,191 @@
-from pathlib import Path
+from typing import List
+from langchain_text_splitters import MarkdownHeaderTextSplitter
+from langchain_fireworks import ChatFireworks
+from langchain_fireworks import FireworksEmbeddings
+from kinconnect_api.config import MONGO_CONNECTION_STRING, RAW_DATA_DIR, load_dotenv
+import pandas as pd
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+from textwrap import dedent
 
-import typer
-from loguru import logger
-from tqdm import tqdm
+import pandas as pd
+from langchain_core.pydantic_v1 import BaseModel, Field
+from typing import List, Dict, Callable, Union
 
-from kinconnect_api.config import PROCESSED_DATA_DIR
+load_dotenv()
 
-app = typer.Typer()
-
-
-@app.command()
-def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    input_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
-    output_path: Path = PROCESSED_DATA_DIR / "features.csv",
-    # -----------------------------------------
-):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger.info("Generating features from dataset...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
-    logger.success("Features generation complete.")
-    # -----------------------------------------
+class CareerEntry(BaseModel):
+    company: str = Field(..., title="Company they worked at")
+    title: str = Field(..., title="Title of the role they held")
+    description: str = Field(..., title="Description of the role they held")
+    start_date: str = Field(..., title="Start date of the role")
+    end_date: str = Field(..., title="End date of the role")
 
 
-if __name__ == "__main__":
-    app()
+class ProjectEntry(BaseModel):
+    title: str = Field(..., title="Title of the project")
+    description: str = Field(..., title="Description of the project")
+
+
+class ProfileModel(BaseModel):
+    name: str = Field(..., title="Name of the person")
+    honors: list[str] = Field(None, title="Honors, Awards and recognition they have received in life")
+    interests: list[str] = Field(..., title="Interests and current focus of theirs the work or the event")
+    skills: list[str] = Field(..., title="Skills they have")
+    career: List[CareerEntry] = Field(..., title="Career history of the person")
+    past_projects: List[ProjectEntry] = Field(..., title="Projects they have worked on")
+    elevator_pitch: str = Field(..., title="Elevator pitch for the person for the event")
+
+
+
+class QAPair(BaseModel):
+    question: str = Field(..., title="Question")
+    answer: str = Field(..., title="Answer")
+
+class FormSubmission(BaseModel):
+    qa_pairs: List[QAPair] = Field(..., title="QA pairs")
+
+
+def get_career(profile):
+    prompt_str = dedent("""
+You are a data generation app which creates profiles for participants at a hackathon. 
+Given their form submission which asks them a few questions. Create a profile. 
+Find all the relevant details about their career.
+
+<form_submission>                                   
+{submission}
+</form_submission>
+""")
+    mixtral_llm = ChatFireworks(
+        model="accounts/fireworks/models/mistral-7b-instruct-v3")
+
+    career_llm = mixtral_llm.with_structured_output(CareerEntry)
+
+    prompt = ChatPromptTemplate.from_template(prompt_str)
+    chain = prompt | career_llm
+    return chain.invoke({"submission": profile})
+
+
+def get_profile_details(profile):
+    prompt_str = dedent("""
+You are a data generation app which creates profiles for participants at a hackathon. 
+Given their form submission which asks them a few questions. Create a profile. 
+Find all the relevant details about their profile. Find their skills, accolades, honors and current interests.
+Generate a name for the person. Make the name diverse and real representation of people working in the silicon valley with the relevant description of their form submission.
+                        
+<form_submission>                                   
+{submission}
+</form_submission>
+""")
+    # Define a Pydantic model for the profile
+    class ProfileDetails(BaseModel):
+        honors: List[str] = Field(None, title="Honors, Awards and recognition they have received in life")
+        interests: List[str] = Field(..., title="Interests and current focus of theirs the work or the event")
+        skills: List[str] = Field(..., title="Skills they have")
+        name: str = Field(..., title="Name of the person")
+        elevator_pitch: str = Field(..., title="Elevator pitch for the person for the event")
+        
+    gpt35_llm =  ChatOpenAI(model="gpt-3.5-turbo")
+
+    career_llm = gpt35_llm.with_structured_output(ProfileDetails)
+
+    prompt = ChatPromptTemplate.from_template(prompt_str)
+    chain = prompt | career_llm
+    return chain.invoke({"submission": profile})
+
+
+def get_project(profile):
+    prompt_str = dedent("""
+You are a data generation app which creates profiles for participants at a hackathon. 
+Given their form submission which asks them a few questions. Create a profile. 
+Find all the relevant details about their past projects and portfolio.
+
+<form_submission>                                   
+{submission}
+</form_submission>
+""")
+    mixtral_llm = ChatFireworks(
+        model="accounts/fireworks/models/mistral-7b-instruct-v3")
+
+    career_llm = mixtral_llm.with_structured_output(ProjectEntry)
+
+    prompt = ChatPromptTemplate.from_template(prompt_str)
+    chain = prompt | career_llm
+    return chain.invoke({"submission": profile})
+
+
+def get_full_profile(form_submission):
+    
+    
+    founder_profile = f'\n\n'.join(f"## {pair.question}: \n {pair.answer}" for pair in form_submission)
+    career = get_career(founder_profile)
+    project = get_project(founder_profile)
+    profile_details = get_profile_details(founder_profile)
+    profile = ProfileModel(
+            name=profile_details.name,
+            honors=profile_details.honors,
+            interests=profile_details.interests,
+            skills=profile_details.skills,
+            career=[
+                CareerEntry(
+                    company=career.company,
+                    title=career.title,
+                    description=career.description,
+                    start_date=career.start_date,
+                    end_date=career.end_date
+                )
+            ],
+            past_projects=[
+                ProjectEntry(
+                    title=project.title,
+                    description=project.description
+                )
+            ],
+            elevator_pitch=profile_details.elevator_pitch
+        )
+    return profile
+
+def get_profile_doc(profile: dict):
+    past_projects_str = '\n'.join(get_project(item) for item in profile['past_projects'])
+    
+    career_str = '\n'.join(get_career(item) for item in profile['career'])
+
+    prodile_doc = dedent(f'''
+
+    This is the profile of {profile['name']}
+
+    # Elevator Pitch of the person
+    {profile['elevator_pitch']}
+
+    # Career History and work experience of the person
+    {career_str}
+
+    # Past Projects and Portfolio
+    {past_projects_str}
+
+    # Skillset they have which will be useful for them
+    {profile['skills']}
+
+    # Their current focus and interests to match with relevant people
+    {profile['interests']}
+
+
+    # Awards and Honors they have won over their life
+    {profile['honors']}
+    ''')
+    headers_to_split_on = [
+        ("#", "Header 1"),
+        ("##", "Header 2"),
+        ("###", "Header 3"),
+    ]
+
+    markdown_splitter = MarkdownHeaderTextSplitter(headers_to_split_on=headers_to_split_on)
+    md_header_splits = markdown_splitter.split_text(prodile_doc)
+    vector_search_index = "embedding_idx"
+    vector_search = MongoDBAtlasVectorSearch.from_documents(
+        documents = md_header_splits,
+        embedding = FireworksEmbeddings(model="nomic-ai/nomic-embed-text-v1.5"),
+        collection = profiles_collection,
+        index_name = vector_search_index
+    )
+    return vector_search
