ba7d4d56c6ef8bbb38bea267633d88543b8ae28d
rob <rob_abcba@yahoo.com>
2024-06-14 17:20:35 -0700

    mine

--- kinconnect-frontend/src/ProfileEditor.js
+++ kinconnect-frontend/src/ProfileEditor.js
@@ -1,21 +1,22 @@
-import React, { useState, useEffect } from 'react'
-import { search } from './net'
+import React, { useState, useEffect } from "react";
+import { search } from "./net";
 
 export default function ProfileEditor({ profile = {}, onSearchResults }) {
-    const [ spinner, setSpinner ] = useState(false);
-    const [ editedProfile, setEditedProfile ] = useState();
+  const [spinner, setSpinner] = useState(false);
+  const [editedProfile, setEditedProfile] = useState();
 
-    const reload = () => {
-        if( !profile )
-            setEditedProfile(undefined);
-        else {
-            const copy = JSON.parse(JSON.stringify(profile));
-            setEditedProfile(copy);
-        }
+  const reload = () => {
+    if (!profile) setEditedProfile(undefined);
+    else {
+      const copy = JSON.parse(JSON.stringify(profile));
+      setEditedProfile(copy);
     }
-    useEffect( () => { reload() }, [profile] );
+  };
+  useEffect(() => {
+    reload();
+  }, [profile]);
 
-/*
+  /*
         name: str = Field(..., title="Name of the person")
     honors: str = Field(None, title="Honors, Awards and recognition they have recieved in life")
     interests: str = Field(..., title="Interests and current focus of theirs the work or the event")
@@ -24,48 +25,47 @@ export default function ProfileEditor({ profile = {}, onSearchResults }) {
     past_projects: List[ProjectEntry] = Field(..., title="Projects they have worked on")
     elevator_pitch: str = Field(..., title="Elevator pitch for the person fpr the event")
 */
-    const onFieldUpdate = (delta) => {
-        const updated = {...editedProfile,...delta};
-        setEditedProfile(updated);
+  const onFieldUpdate = (delta) => {
+    const updated = { ...editedProfile, ...delta };
+    setEditedProfile(updated);
+  };
+
+  const onSearch = async () => {
+    if (spinner) return;
+
+    setSpinner(true);
+    try {
+      const searchResults = await search({ profile: editedProfile });
+      onSearchResults(searchResults);
+    } catch (err) {
+      console.log("onSearch error", err);
     }
+    setSpinner(false);
+  };
 
-    const onSearch = async () => {
-        if( spinner )
-            return;
+  return (
+    <div>
+      <h2>Profile Editor</h2>
 
-        setSpinner(true);
-        try {
-            const searchResults = await search({ profile: editedProfile });
-            onSearchResults( searchResults );
-        } catch(err) {
-            console.log('onSearch error',err);
-        }
-        setSpinner(false);
-    }
+      <input
+        type="text"
+        placeholder="name"
+        value={editedProfile.name || ""}
+        onChange={(event) => {
+          onFieldUpdate({ name: event.target.value });
+        }}
+      />
 
-    return (
-        <div>
-            <h2>Profile Editor</h2>
+      <input
+        type="text"
+        placeholder="honors"
+        value={editedProfile.honors || ""}
+        onChange={(event) => {
+          onFieldUpdate({ honors: event.target.value });
+        }}
+      />
 
-            <input
-                type="text"
-                placeholder="name"
-                value={editedProfile.name}
-                onChange={(event)=>{
-                    onFieldUpdate({name:event.target.value})
-                }}
-            />
-
-            <input
-                type="text"
-                placeholder="honors"
-                value={editedProfile.honors || ''}
-                onChange={(event)=>{
-                    onFieldUpdate({honors:event.target.value})
-                }}
-            />
-
-            <button onClick={onSearch}>Search</button>
-        </div>
-    )
+      <button onClick={onSearch}>Search</button>
+    </div>
+  );
 }
